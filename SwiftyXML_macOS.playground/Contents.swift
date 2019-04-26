585bf14565c4fc0e6409a8c94d3714bdf3eb3e6d
Thomas Bonk <thomas@meandmymac.de>
2019-04-26 15:14:09 +0200

    recreated project to be compatible with swift package manager

--- SwiftyXML_macOS.playground/Contents.swift
+++
@@ -1,7 +0,0 @@
-import Cocoa
-import SwiftyXML
-
-let xml = XML(url: #fileLiteral(resourceName: "sample.xml"))!
-print(xml.toXMLString())
-
-xml["result"][0]["address_component"][3]["short_name"].stringValue
